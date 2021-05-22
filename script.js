fadc1fa4fa6003c98ab601370f90af9347b53a6d
shivangshah42 <38208120+shivangshah42@users.noreply.github.com>
2021-05-23 00:47:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const banner=document.getElementsByClassName('banner')[0];
+const block=document.getElementsByClassName('blocks');
+
+ for(var i=1;i<400;i++){
+  banner.innerHTML += "<class class='blocks'></class>";
+ }
